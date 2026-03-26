3cc428ba4ab6122906af0f8601b46b37f9fdc30a
Franco Rutigliano <francorutigliano11@gmail.com>
2026-03-26 20:59:40 -0300

    add Rlocking mechanism to book method for read thread safety

--- internal/booking/concurrent_store.go
+++ internal/booking/concurrent_store.go
@@ -27,6 +27,8 @@ func (m *ConcurrentStore) Book(book Booking) error {
 }
 
 func (m *ConcurrentStore) ListBookingsByMovie(movieID string) []Booking {
+	m.RLock()
+	defer m.RUnlock()
 	var result []Booking
 	for _, booking := range m.bookings {
 		if booking.MovieID == movieID {
