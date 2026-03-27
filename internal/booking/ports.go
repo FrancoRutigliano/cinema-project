7f412afd78bd22451f53283ba0e43c5c80f66447
Franco Rutigliano <francorutigliano11@gmail.com>
2026-03-27 17:31:46 -0300

    extend two methods on booking store

--- internal/booking/ports.go
+++ internal/booking/ports.go
@@ -1,6 +1,11 @@
 package booking
 
+import "context"
+
 type BookingStore interface {
 	Book(b Booking) (Booking, error)
 	ListBookingsByMovie(movieID string) ([]Booking, error)
+
+	Confirm(ctx context.Context, sessionID, userId string) (Booking, error)
+	Release(ctx context.Context, sessionID, userId string) error
 }
