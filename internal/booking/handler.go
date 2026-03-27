ee1d7197264a2e8ec519d3fc96c947e9d79257e1
Franco Rutigliano <francorutigliano11@gmail.com>
2026-03-27 15:25:07 -0300

    add response error estructure and return

--- internal/booking/handler.go
+++ internal/booking/handler.go
@@ -2,6 +2,8 @@ package booking
 
 import (
 	"booking-concurrent/internal/utils"
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -19,6 +21,15 @@ func (h *handler) HoldSeats(w http.ResponseWriter, r *http.Request) {
 	movieID := r.PathValue("movieID")
 	seatID := r.PathValue("seatID")
 
+	var req holdRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		utils.WriteJSON(w, http.StatusInternalServerError, ErrorResponse{
+			Error: fmt.Sprintf("decoder failed: %w", err),
+		})
+		return
+	}
+
+	h.svc.store.Book(Booking{})
 }
 
 func (h *handler) ListSeats(w http.ResponseWriter, r *http.Request) {
@@ -26,7 +37,10 @@ func (h *handler) ListSeats(w http.ResponseWriter, r *http.Request) {
 
 	bookings, err := h.svc.store.ListBookingsByMovie(movieID)
 	if err != nil {
-		utils.WriteJSON(w, http.StatusInternalServerError, nil)
+		utils.WriteJSON(w, http.StatusInternalServerError, ErrorResponse{
+			Error: err.Error(),
+		})
+		return
 	}
 
 	seats := make([]seatInfo, 0, len(bookings))
