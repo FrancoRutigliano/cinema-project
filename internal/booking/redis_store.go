88a2effdcacffe90467a76b6ac19719e5c421ada
Franco Rutigliano <francorutigliano11@gmail.com>
2026-03-26 21:13:06 -0300

    add default hold TTL

--- internal/booking/redis_store.go
+++ internal/booking/redis_store.go
@@ -1,9 +1,14 @@
 package booking
 
 import (
+	"time"
+
 	"github.com/redis/go-redis/v9"
 )
 
+const defaultHoldTTL = 2 * time.Minute
+
+// Redis store implementa sesiones basadas en reservas respaldadas por redis.
 type RedisStore struct {
 	rdb *redis.Client
 }
