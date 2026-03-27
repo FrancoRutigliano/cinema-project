513bcbc0ec709b4f5d6f3122570df991d2c1ef93
Franco Rutigliano <francorutigliano11@gmail.com>
2026-03-26 21:25:06 -0300

    implement basic hold function for persistance

--- internal/booking/redis_store.go
+++ internal/booking/redis_store.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
-const defaultHoldTTL = 2 * time.Minute
+const (
+	defaultHoldTTL = 2 * time.Minute
+)
 
 // Redis store implementa sesiones basadas en reservas respaldadas por redis.
 type RedisStore struct {
@@ -23,4 +26,18 @@ func NewRedisStore(rdb *redis.Client) *RedisStore {
 // Ayuda con el reverse-lookup a buscar sesión por su ID
 func sessionKey(id string) string {
 	return fmt.Sprintf("session:%s", id)
+}
+
+func (r *RedisStore) hold(book Booking) (Booking, error) {
+	id := uuid.New().String()
+	now := time.Now()
+
+	return Booking{
+		ID:        id,
+		MovieID:   book.MovieID,
+		SeatID:    book.SeatID,
+		UserID:    book.UserID,
+		Status:    StatusHeld,
+		ExpiresAt: now.Add(defaultHoldTTL),
+	}, nil
 }
